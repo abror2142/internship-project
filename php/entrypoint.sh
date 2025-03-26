2e6fd8ffcb94d91e7b69ed0f4175e4975fe3c7cb
Abrorbek Ismoilov <107830282+abror2142@users.noreply.github.com>
2025-03-26 20:12:53 +0500

    Separating frontend and backend.

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 php artisan migrate --force
 
-chown -R www-data:www-data /app /app/vendor
-chmod -R 775 /app /app/vendor
+chown -R www-data:www-data /api /api/vendor
+chmod -R 775 /api /api/vendor
 
 exec php-fpm
