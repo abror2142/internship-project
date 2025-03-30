c6f642e40e2d24a80227005d17790c907b7b2db5
Abrorbek Ismoilov <107830282+abror2142@users.noreply.github.com>
2025-03-30 07:25:42 +0500

    Adding JWT authentication.

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/logout',[AuthController::class, 'logout'])->middleware(['auth:api']);
+Route::post('/me',[AuthController::class, 'me']);
+Route::post('/refresh',[AuthController::class, 'refresh']);
