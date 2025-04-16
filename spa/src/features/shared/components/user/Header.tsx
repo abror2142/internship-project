11516403f33b96d19c26c4fbbbe925d8b0bfb4d2
Abrorbek Ismoilov <107830282+abror2142@users.noreply.github.com>
2025-04-16 16:22:03 +0500

    User Image view

--- spa/src/features/shared/components/user/Header.tsx
+++ spa/src/features/shared/components/user/Header.tsx
@@ -22,15 +22,25 @@ function Header () {
 
             <SearchBar />
             
-            <div className="flex gap-4 dark:text-dark-text-highlighted items-center">
+            <div className="flex gap-6 dark:text-dark-text-highlighted items-center">
                 <DarkModeToggler />
                 { 
                     user 
-                    ? <div className="flex gap-2 items-center ">
-                        <p className="underline">{user?.name}</p>
-                        <button onClick={() => logout()}
-                                className="px-3 py-1.5 bg-gray-300 dark:bg-dark-blue rounded-sm dark:hover:bg-blue-900"
-                            >Logout</button>
+                    ? <div className=" flex gap-2 items-center ">
+                        <div className="relative group w-8 h-8 rounded-full bg-dark-blue-light">
+                            {
+                                user?.image
+                                ? <img src={user?.image} className="w-8 h-8 rounded-full"/>
+                                : <p className="text-lg">{user.name.split(' ').slice(0, 2).map(part => part[0]).join('').toUpperCase()}</p>
+                            }
+                            <div className="text-sm hidden min-w-30 group-hover:flex flex-col gap-2 absolute top-8 -right-2 px-4 py-2 bg-dark-bg border dark:border-dark-border">
+                                <Link to={'/profile'} className="text-center py-1 bg-gray-300 dark:bg-dark-blue rounded-sm dark:hover:bg-blue-900">Profile</Link>
+                                <button 
+                                    onClick={() => logout()}
+                                    className="py-1 bg-gray-300 dark:bg-dark-blue rounded-sm dark:hover:bg-blue-900"
+                                >Logout</button>
+                            </div>
+                        </div>
                         
                     </div>
                     : <div className="flex gap-2 items-center">
